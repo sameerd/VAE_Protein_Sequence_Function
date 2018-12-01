4378102a71375462cd5c2d1a5a29e27d0ca28a28
sameerdcosta@gmail.com <sameerdcosta@gmail.com>
2018-12-01 15:35:25 -0600

    Extracting double mutations

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ We are looking at Variational AutoEncoders for Protein Sequence-Function data
 
 #### Papers/Resources
 We are mainly following the following paper
-[Variational auto-encoding of protein sequences](https://arxiv.org/abs/1712.03346)
+1. [Variational auto-encoding of protein sequences](https://arxiv.org/abs/1712.03346)
 Sinai, Sam; Kelsic, Eric; Church, George M.; Nowak, Martin A.
 
-This Youtube video also gives an overview of the big picture
+1. Youtube video with an overview / big picture 
 [MIA: Debora Marks, Structure & fitness from genomics sequences; John Ingraham & Adam Riesselman](https://youtu.be/97q2wtoquQk)
--- python/scripts/preprocess_bgl3.py
+++ python/scripts/preprocess_bgl3.py
@@ -6,9 +6,11 @@ import functools
 import pandas as pd
 
 data_dir = "data"
+working_dir = os.path.join(data_dir, "working")
 
 input_dir = os.path.join(data_dir, "seq-fcn-data/Bgl3_random_mutagenesis")
-output_csv = os.path.join(data_dir, "Bgl3_single_mutations.csv")
+single_output_file = os.path.join(working_dir, "Bgl3_single_mutations.tsv")
+double_output_file = os.path.join(working_dir, "Bgl3_double_mutations.tsv")
 
 files = {'unsorted':'unlabeled_Bgl3_mutations.txt',
          'positive':'positive_Bgl3_mutations.txt',
@@ -28,6 +30,7 @@ def extract_double_mutations(filename, *args, **kw):
 def convert_mutation_list_to_df(mutations_list, count_label):
     """ Take in a list of mutations and count how many of there in a dataframe
         The number of counts appears in the count_label column
+        This works for both single and double mutations
     """
     df = pd.Series(mutations_list)
     unique_counts = df.groupby(df).nunique()
@@ -37,15 +40,46 @@ def convert_mutation_list_to_df(mutations_list, count_label):
     retdf.reset_index(drop=True, inplace=True)
     return(retdf)
 
-# Create pandas dataframes for each file which counts the numbers of mutations
+
+## Single Mutations
+# Create dataframes for each file which counts the numbers of mutations
 single_mutations = {k:convert_mutation_list_to_df(
     extract_single_mutations(files[k]), 
     count_label=k) for k in files.keys()}
 
 # Now lets merge all the dataframes together
-df = functools.reduce(lambda left, right: 
+single_df = functools.reduce(lambda left, right: 
         pd.merge(left, right, on="mutations", how="outer"),
         single_mutations.values())
 
 # Save it as csv
-df.to_csv(output_csv)
+single_df.to_csv(single_output_file, sep="\t")
+
+## Double Mutations
+double_mutations = {k:convert_mutation_list_to_df(
+    extract_double_mutations(files[k]), 
+    count_label=k) for k in files.keys()}
+
+
+# Check that all the double mutations are indexed correctly
+# Looks like this always passes so there is no need to always check this
+for key, df in double_mutations.items():
+  # Extract the first and the second index where the mutations occur
+  y = df["mutations"].str.extractall(
+          r"\D(?P<index1>[\d]*)\D,\D(?P<index2>[\d]*)\D")
+  y.index1 = pd.to_numeric(y.index1)
+  y.index2 = pd.to_numeric(y.index2)
+  # check that the first mutation is never larger than or equal 
+  # to the second
+  if y.loc[y.index1 >= y.index2].size != 0:
+    print("The ordering of the double mutations in ", key, 
+          " is not correct!")
+
+# Now lets merge all the dataframes together
+double_df = functools.reduce(lambda left, right: 
+        pd.merge(left, right, on="mutations", how="outer"),
+        double_mutations.values())
+
+double_df.to_csv(double_output_file, sep="\t")
+
+
